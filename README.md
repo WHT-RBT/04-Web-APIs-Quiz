75f7c247a7ca0d12b260d5030627c5980d0e022f
whtrbt <rlj.castillosb@yahoo.com>
2023-05-11 15:31:30 -0700

    main

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # 04-Web-APIs-Quiz
+
+USER STORY:
+
+This application is a web-based timed quiz on JavaScript fundamentals that stores high scores.
+
+The user will be greeted with a start button and high score button with instructions for the quiz. 
+
+The user will need to click the START button to begin the timed quiz.
+
+Clicking the Start button will hide the Start and High Scores button and quiz instructions, beginnig the quiz.
+
+The timer will then be on display in the top left corner of the screen, with timer ticking audio that will play on a loop throughout the quiz.
+
+The user will have 40 seconds to complete the 5 question quiz.
+
+The user will then be presented with their first question with a list of four multiple answers.
+
+When the user answers a question incorrectly, they will be deducted 10 seconds with an alert notifying them of their incorrect answer, as well as an incorrect buzzer sound.
+
+When the user answers a question correctly, they will be given 5 seconds with an alert notifying them of their correct answer, and a correct buzzer sound.
+
+Quiz ends when all questions are answered, or the timer reaches 0.
+
+The user will then have the opportunity to save their initials and score, that will display on the bottom left of the screen.
+
+APPLICATIONS USED:
+
+    -HTML
+    -CSS
+    -JavaScript
+
+The following image demonstrates an image of the application's functionality:
+![Quiz Image](Assets/images/quiz.png)
--- script.js
+++ script.js
@@ -1,119 +1,122 @@
 // variables
-var question = 
-var answers = 
-var correctNotification = Audio("Assets\audio\correctanswer.wav")
-var wrongNotification = Audio("Assets\audio\wronganswer.wav")
-var tickingTimer = Audio("Assets\audio\tickingtimer.mp3")
+let secondsRemaining;
+let currentQuestion;
+var score = 0;
+const userScore = document.getElementById("score");
+const highscoresButton = document.getElementById("highScores");
+const instructions = document.getElementsByClassName("instructions");
 
 
-startQuiz.onclick = startTimer;
+//start quiz
+function startQuiz() {
+  console.log("Starting")
+  hide()
+  secondsRemaining = 25
+  startTimer()
+  currentQuestion = 0
+  displayQuestion()
+}
 
+//WHEN start button is clicked, high score/start/instructions is hidden and ticking timer begins audio loop
+function hide() {
+  startButton.classList.add("hide")
+  highscoresButton.style.display = "none"
+  instructions[0].classList.add("hide")
+}
 function startTimer() {
-  displayQuestion();
-
-timer = setInterval(function() {
-  secondsRemaining -= 1;
-  console.log(secondsRemaining);
-
-var timerDisplay = document.getElementById("theTimer");
-  timerDisplay.textContent = secondsRemaining;
-
-if (secondsRemaining === 0) {
-  clearInterval(timer);
-  window.location.href = "highscore.html";
-  }
+  const tickingTimer = new Audio("./Assets/audio/tickingtimer.mp3");
+  const timerDisplay = document.getElementById("timer");
+  timer = setInterval(function () {
+    secondsRemaining -= 1;
+    timerDisplay.textContent = secondsRemaining;
+  tickingTimer.play()
+    if (secondsRemaining <= 0) {
+      clearInterval(timer);
+      endQuiz(); 
+      console.log();
+    }
   }, 1000);
 }
 
-//quiz questions
-var questions = [
+//Display of quiz questions
+function displayQuestion() {
+
+    document.getElementById("question-prompt").innerHTML = questionBank[currentQuestion].prompt
+
+  var choices = questionBank[currentQuestion].answers;
+  var answersHolder = document.getElementById("answers-holder")
+  for (var i = 0; i < choices.length; i++) {
+    var choicesElement = document.createElement("button");
+    choicesElement.textContent = choices[i];
+    choicesElement.addEventListener("click",isCorrectAnswer)
+    answersHolder.append(choicesElement)
+  }
+}
+
+//10 seconds removed for incorrect answers
+//userScore and save/display scoresit comm
+function isCorrectAnswer(event) {
+  console.log(event.target);
+
+//need to add sound effects for wrong or correct answers
+const correctaudio = new Audio("./Assets/audio/correctaudio.wav");
+const wrongaudio = new Audio("./Assets/audio/wrongaudio.wav");
+
+// create a variable that is going to hold the text content of event target
+// compare that git variable to what the correct answer is
+
+  if (event) {
+    alert("Correct! Good Job! +5 seconds!");
+    correctaudio.play();
+  }
+  else {
+    alert("INCORRECT! -10 SECONDS!");
+    wrongaudio.play();
+  }
+}
+// function addTime() {
+//   secondsRemaining += 5;
+// }
+// function removeTime() {
+//   secondsRemaining -= 10;
+// }
+// function getScore() {
+//   return secondsRemaining;
+// }
+// localStorage.setItem("Score", secondsRemaining);
+
+// function getUserInfo() {
+// }
+// function saveScore() {
+// }
+
+let questionBank = [
   {
-  question: "How do you print 'Hello World' in the console?",
-  answers: {
-  a: "msgbox.('Hello World')",
-  b: "alertbox.('Hello World')",
-  c: "log('Hello World').console",
-  d: "console.log('Hello World')",
-  correctAnswer: "d",
+    prompt: "How do you print 'Hello World' in the console?",
+    answers: ["msgbox.('Hello World')", "alertbox.('Hello World')", "log('Hello World').console", "console.log('Hello World')"],
+    correctAnswer: ["console.log('Hello World')"],
   },
   {
-  question: "What type of Pop up boxes are available in JavaScript?",
-  a: "Alert",
-  b: "Confirm",
-  c: "Prompt",
-  d: "All of the above",
-  correctAnswer: "c",
+  prompt: "What type of Pop up boxes are available in JavaScript?",
+  answers: ["Alert", "Confirm", "Prompt", "All of the above"],
+  correctAnswer: ["All of the above"],
   },
   {
-  question: "Commonly used data types DO NOT include:",
-  a: "Booleans",
-  b: "Strings",
-  c: "Alerts",
-  d: "Numbers",
-  correctAnswer: "a",
+  prompt: "Commonly used data types DO NOT include:",
+  answers: ["Booleans", "Strings", "Alerts", "Numbers"],
+  correctAnswer: ["Booleans"],
   },
   {
-  question: "Inside which file do we use to style our webpage?",
-  a: "JavaScript file",
-  b: "HTML file",
-  c: "CSS file",
-  d: "None of the above",
-  correctAnswer: "c",
+  prompt: "Inside which file do we use to style our webpage?",
+  answers: ["JavaScript file", "HTML file", "CSS file", "None of the above"],
+  correctAnswer: ["CSS file"],
   },
   {
-  question: "Arrays in JavaScript can be used to store ____.",
-  a: "Numbers and strings",
-  b: "Other arrays",
-  c: "Booleans",
-  d: "All of the above",
-  correctAnswer: "d",
+  prompt: "Arrays in JavaScript can be used to store ____.",
+  answers: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+  correctAnswer: ["All of the above"],
   },
 ];
 
-// display questions
-function displayQuestion() 
-  document.getElementById("question").innerHTML ="";
-
-  var question = document.createElement("h2");
-  var currentQuestion = questions[currentIndex].title;
-  titleElement.textContent = currentQuestion;
-
-  var choices = questions[currentIndex].choices;
-
-for (var i = 0; i < choices.length; i++) {
-  var choicesElement = document.createElement("button");
-  choicesElement.textContent = choices[i];
-  choicesElement.onclick = isCorrectAnswer;
-  }
-
-// sound effects for wrong or correct answers
-if (isCorrect) {
-  ("#feedback").html("Correct! Good Job!").show();
-  correctNotification.play();
-}
-else {
-  ("#feedback").html("WRONG! Better luck on the next one.").show();
-  wrongNotification.play();
-}
-
-//10seconds removed for incorrect answers
-function isCorrectAnswer() {
-  var answer = questions[currentIndex].answer;
-  userAnswer = this.innerHTML;
-
-function addTime() {
-  secondsRemaining += 10;
-}
-function removeTime() {
-  secondsRemaining -= 10;
-}
-function getScore() {
-  return secondsRemaining;
-}
-localStorage.setItem("Score", secondsRemaining);
-
-function getUserInfo() {
-}
-function saveScore() {
-}
-}
+const startButton = document.querySelector("#startQuiz");
+startButton.addEventListener("click", startQuiz)
